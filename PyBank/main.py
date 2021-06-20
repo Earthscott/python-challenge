57242bb701278345287d16a1e450a0d8968a7a2d
Scott Job <earthscott@gmail.com>
2021-06-20 18:11:38 -0400

    Update PyBank to reflect intended calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,17 @@
 import os
 import csv
 
-# Format currency with negative sign in front of dollar sign
+# Format float as currency with negative sign in front of dollar sign
 def format_currency(value):
     curr_str = f"${abs(value):,.2f}"
     if value < 0:
         curr_str = "-" + curr_str
     return curr_str
 
-
+# Input file has columns: Date, Profit/Losses
+    # Date format "MMM-YYYY"
+    # 'Profit/Losses' represents profit or loss for the given month-year
+    
 # Set path to input file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
@@ -23,7 +26,9 @@ with open(csvpath, 'r') as csvfile:
 
     # Initialize target variables
     month_count = 0
-    net_pl = 0.00
+    net_pl = 0.0
+    prev_pl = 0.0
+    delta_list = []
     gr_incr = ["mmm-yyyy", 0.00]
     gr_decr = ["mmm-yyyy", 0.00]
 
@@ -31,33 +36,37 @@ with open(csvpath, 'r') as csvfile:
     for row in csvreader:
 
         # Convert current monthly profit/loss to float
-        row[1] = float(row[1])
+        pl = float(row[1])
 
         # Update record count
         month_count += 1
 
         # Update total profit/loss
-        net_pl += row[1]
+        net_pl += pl
 
-        # Test and set max monthly increase and max decrease
-        if row[1] > gr_incr[1]:
-            gr_incr = [row[0], row[1]]
-        if row[1] < gr_decr[1]:
-            gr_decr = [row[0], row[1]]
+        # Calculate month-to-month difference in profit/loss
+        if month_count > 1:                   # Skip first record
+            delta = pl - prev_pl              # Calculate difference
+            delta_list.append(delta)          # Add difference to list for mean calculation
+            if delta > gr_incr[1]:            # Identify greatest increase and store mo/yr
+                gr_incr = [row[0], delta]
+            if delta < gr_decr[1]:            # Identify greatest decrease and store mo/yr
+                gr_decr = [row[0], delta]
+        
+        # Store previous profit/loss for difference calculation
+        prev_pl = pl
 
-# Calculate mean monthly profit/loss
-mean_change = net_pl / month_count
+# Calculate mean change in month-to-month profit/loss difference
+delta_mean = sum(delta_list) / len(delta_list)
 
 # List to hold analysis output text
 output = ["Financial Analysis",
-    "--------------------------------------------------------------",
-    f"Total months: {month_count}",
+    "-------------------------------------------------------------------------",
+    f"Months in analysis: {month_count}",
     f"Net profit/loss: {format_currency(net_pl)}",
-    f"Average monthly profit/loss: {format_currency(mean_change)}",
-    f"Greatest monthly profit {format_currency(gr_incr[1])} occurred during {gr_incr[0]}",
-    f"Greatest monthly loss {format_currency(gr_decr[1])} occurred during {gr_decr[0]}",
-    ]
-
+    f"Mean of month-to-month profit/loss differences: {format_currency(delta_mean)}",
+    f"Greatest month-to-month profit/loss increase: {format_currency(gr_incr[1])}, {gr_incr[0]}",
+    f"Greatest month-to-month profit/loss decrease: {format_currency(gr_decr[1])}, {gr_decr[0]}",]
 
 # Print results to console
 for row in output:
@@ -68,8 +77,3 @@ outfile = os.path.join('Analysis', "PyBank_output.txt")
 with open(outfile, 'w') as file1:
     for row in output:
         file1.write(f"{row}\n")
-
-
-
-
-
