6311cab67d014c735a707b6040c5df2cc4fd6d2a
Scott Job <earthscott@gmail.com>
2021-06-23 12:02:12 -0400

    Project completed

--- PyPoll/bonus.py
+++ PyPoll/bonus.py
@@ -1,9 +1,11 @@
 import os
 import csv
 import copy
-from re import split
-import numpy as np
 from prettytable import PrettyTable
+
+# ====================================================
+# Bonus analysis: Tally votes by county and candidate
+# ====================================================
 
 # Input file has columns: Voter ID, County, Candidate
 # Each record represents one vote for the identified candidate
@@ -36,72 +38,100 @@ with open(csvpath, 'r') as csvfile:
 # Create independent copy of dictionary
 test_dict = copy.deepcopy(vote_count2)
 
-# Delete some items so a few county-candidate combinations are undefined
+# Throw a wrench in the works. Delete some items so a few county-candidate 
+#    combinations are undefined. This forces the analysis to deal with 
+#    undefined combinations of county/candidte.
 del test_dict['Trandee']["O'Tooley"]
 del test_dict['Trandee']['Li']
 del test_dict['Queen']['Khan']
 del test_dict['Bamoo']['Khan']
 
+# List of unique counties
 counties = sorted(test_dict.keys())
 
-candidates = []
+# Make list of unique candidates and tally total votes by county
+candidate_set = set()
 votes_bycounty = {}
-
+# Note that 'candidate_dict' is a dictionary, not a value
 for county, candidate_dict in test_dict.items():
-    candidates.extend(list(candidate_dict.keys()))
+    # Add only unique items to set 'candidates_set'
+    candidate_set.update(candidate_dict.keys())
+    # candidate_dict.values converted to list to allowing summing values
     votes_bycounty[county] = sum(list(candidate_dict.values()))
 
-x = np.array(candidates)
-candidates = sorted(np.unique(x))
+# Convert 'candidate_set' to sorted list
+candidates = sorted(candidate_set)
 
+# Tally total votes for each candidate
+#    Initialize dictionary with count=0 for each candidate
 votes_bycandidate = {key: 0 for key in candidates}
-
+# Note that 'candidate_dict' is a dictionary, not a value
 for candidate_dict in test_dict.values():
     for candidate in candidate_dict.keys():
-        votes_bycandidate[candidate] += candidate_dict.get(candidate,0)
+        votes_bycandidate[candidate] += candidate_dict[candidate]
 
+# Sum total votes in election
 total_votes = sum(votes_bycounty.values())
 
-percent_bycandidate = {}
-for candidate in candidates:
-    percent_bycandidate[candidate] = votes_bycandidate[candidate] / total_votes
-
-
+# PrettyTable - "A simple Python library for easily displaying tabular data 
+#    in a visually appealing ASCII table format"
 PTab = PrettyTable()
 
+# Set field names 
 fields = ['County', 'Total Votes']
 fields.extend(candidates)
 PTab.field_names = fields
 
+# Add row for each county with results by candidate
 for county in counties:
-    percents = []
     row = [f"{county}", f"{votes_bycounty[county]:,}"]
+    results = []
     for candidate in candidates:
+        # use dict.get to trap cases where vote=0 for county-candidate 
         count = test_dict[county].get(candidate, 0)
         percent = count / votes_bycounty[county]
-        percents.append(f"{count:,} ({percent:.1%})")
-    row.extend(percents)
+        results.append(f"{count:,} ({percent:.1%})")
+    row.extend(results)
     PTab.add_row(row)
 
+# Add row with totals
 row = ['Total', f"{total_votes:,}"]
-tot_percents = []
+results = []
 for candidate in candidates:
-    tot_percent = percent_bycandidate[candidate]
-    tot_percents.append(f"{tot_percent:.1%}")
-row.extend(tot_percents)
+    count = votes_bycandidate[candidate]
+    percent = count / total_votes
+    results.append(f"{count:,} ({percent:.1%})")
+row.extend(results)
 PTab.add_row(row)
 
+# Right align all fields, then left align 'County' column
 PTab.align = 'r'
 PTab.align['County'] = 'l'
 
+# Modify PrettyTable output string to separate Total row and show winner
+# ----------------------------------------------------------------------
 text_table = PTab.get_string()
+# convert 'text_table' to list of lines
 rows = text_table.split("\n")
+# Extract a "border" row of dashes to use for separating sections of output
 dashrow = rows[0]
+# Insert 'dashrow' to separate Total row from previous results by county
 rows.insert(len(rows) - 2, dashrow)
+# Identify winner name and winner percent of total votes
+winner_name = max(votes_bycandidate, key=votes_bycandidate.get)
+winner_pcnt = f"{(votes_bycandidate[winner_name] / total_votes):.1%}"
+# Start building winner string for insertion
+winner_str = f"| The winner is {winner_name} with {winner_pcnt} of the total vote"
+# Count spaces needed for padding
+spaces = len(dashrow) - len(winner_str) - 1
+# Finish building winner row
+winner_str = winner_str + " "*spaces + "|"
+# Insert winner row and another 'dashrow" at the end
+rows.extend([winner_str, dashrow])
 
+# Print output to console
 for row in rows:
     print(row)
-
 
 
 
--- README.md
+++ README.md
@@ -4,21 +4,16 @@
 
 The objective is to perform a financial analysis of monthly profit or loss over a certain time period. Given an input file with the following fields:
 
-1. Date (format Mmm-yyyy)
-
-2. Profit or loss during that month
+1. Date (format Mmm-yyyy).
+2. Profit or loss during that month.
 
 Find the following:
 
-* The total number of months represented in the dataset
-
-* The cumulative sum of monthly profit/loss values for the entire dataset
-
-* The mean of the month-to-month profit/loss differences (i.e., `PL(i) - PL(i-1)` where _PL_ is the profit/loss and _i_ is the current index of the month
-
-* The greatest increase in month-to-month difference in profit/loss, and the corresponding date
-
-* The greatest decrease in month-to-month difference in profit/loss, and the corresponding date
+* The total number of months represented in the dataset.
+* The cumulative sum of monthly profit/loss values for the entire dataset.
+* The mean of the month-to-month profit/loss differences (i.e., `PL(i) - PL(i-1)` where _PL_ is the profit/loss and _i_ is the current index of the month.
+* The greatest increase in month-to-month difference in profit/loss, and the corresponding date.
+* The greatest decrease in month-to-month difference in profit/loss, and the corresponding date.
 
 Print the results to the terminal and save the results in a text file.
 
@@ -26,24 +21,32 @@ The input file is located in the `PyBank/Resources` folder, and the output is sa
 
 ## PyPoll
 
-The goal is to tally the votes in an election and report on the results. Each record in the input file represents a single vote, and has the following fields:
+The objectives are to tally the votes in an election and report on the results. Each record in the input file represents a single vote, and has the following fields:
 
-1. Voter ID, a unique number representing the voter
-
-2. County, the name of the county in which the vote was cast
-
-3. Candidate, the name of the ccandidate receiving the vote
+1. Voter ID, a unique number representing the voter.
+2. County, the name of the county in which the vote was cast.
+3. Candidate, the name of the ccandidate receiving the vote.
 
 Tabulate the votes for each candidate and report the following:
 
-* The total number of votes cast in the election
+* The total number of votes cast in the election.
 * For each candidate,
-  * Name
-  * The percentage of votes received
-  * The number of votes received
-* The winner of the election 
+  * Name.
+  * The percentage of votes received.
+  * The number of votes received.
+* The winner of the election.
 
 Note that the Voter ID and the County are not needed for this analysis. Print the results to the terminal and save the results in a text file.
 
-The input file is located in the `PyPoll/Resources` folder, and the output is saved in the `PyPoll/Analysis` folder. The script `main.pl` is executed from the `PyPoll` folder.
+The input file is located in the `PyPoll/Resources` folder, and the output is saved in the `PyPoll/Analysis` folder. The script `main.pl` is executed from the `PyPoll` folder. The python script iterates over the input file and stores the vote count in a dictionary with candidate names as keys and vote counts as values.
+
+### Bonus Analysis
+
+The original PyPoll objectives are modified to present the results by county and candidate in a tabular form. The following modifications were made:
+
+* A nested dictionay was used to store the ressults with county as the primary key, candidate name as the nested key, and votes counts by county-candidate combination as values.
+* The original PyPoll dataset was used, but some of the county-candidate combinations were deleted from the dictionary. This was done as a proof-of-concept to address the possibility that a candidate did not receive any votes in a particular county.
+* The python library PrettyTable was used to display results in a visually appealing ASCII table format. PrettyTable has limited options for custimizing the output, so the ASCII table was modified to include additional elements.
+
+The script `bonus.pl` is executed from the `PyPoll` folder, and output is sent to the console only.
 
